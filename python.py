5c29d1d3bb84d25309ce73c1d6cc3fbc92278999
Natarajan-2024-aib <alamelunatraj2007@gmail.com>
2024-11-23 16:31:34 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,17 @@
+#print
+def add():
+    print(2+4)
+add()
+#assing the value
+def python(num1,num2):
+    print(num1+num2)
+python(3,5)      
+
+#return func
+def added(num1,num2):
+    return num1+num2
+print(added(2,4))
+#return assing the value
+def pyadded():
+    return 2+76
+print(pyadded())
